3ee9b7f0ab261e933ffd387fd688618cd0ceea31
黄嘉庆 <657593825@qq.com>
2018-04-19 16:51:03 +0800

    feat: create stage with order

--- server/service/stage.js
+++ server/service/stage.js
@@ -1,29 +1,43 @@
 import mongoose from 'mongoose';
-// import R from 'ramda';
+import R from 'ramda';
 const Stage = mongoose.model('Stage');
 
 export default {
-
   /**
    * 创建阶段
    * @param {object} data 
    */
   async createStage(data) {
     const { order=null } = data;
-    if (!order) {
-      let stages = await Stage.find({
-        _projectId: data._projectId
-      });
-      let stageCount = stages.length;
-      data.order = stageCount+1;
-    }
-
-    let stage = new Stage({
+    let stages = await Stage.find({
+       _projectId: data._projectId
+    });
+    let stage = {
       ...data,
       _stageId: mongoose.Types.ObjectId()
-    });
+    };
+    if (!order) {
+      let stageCount = stages.length;
+      stage.order = stageCount+1;
+    }
+    if (order && R.is(Number, order)) {
+      stages.forEach(async (item) => {
+        if (R.gte(item.order, order)) {
+          let newOrder = item.order+1;
+          await Stage.findOneAndUpdate({
+            _stageId: item._stageId
+          }, {
+            order: newOrder
+          });
+        }
+      });
+    } else {
+      return {
+        message: '创建阶段失败'
+      }
+    }
 
-    return await stage.save();
+    return await new Stage(stage).save();
   },
 
   
